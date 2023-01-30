8d91843a67fe7a0d25259b7561b7e7e02ffb0a63
gustavorteuber <gugareis135@gmail.com>
2023-01-29 23:44:36 -0300

    dashboard-login

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full border-t-2 px-8 py-6 lg:flex justify-between items-center">
+    <p class="mb-2 lg:mb-0">© Copyright Rcidral & gustavorteuber 2023</p>
+
+    <div class="flex">
+      <a href="#" class="hover:text-gray-900">About Us</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
